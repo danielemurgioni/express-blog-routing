ff3bcb78eaec3a349be55e9892f8031f565f0ea0
danielemurgioni <dashomew@gmail.com>
2025-04-17 17:10:31 +0200

    [UPD] Show router and test

--- routers/posts.js
+++ routers/posts.js
@@ -9,4 +9,9 @@ routers.get("/", (req, res)=>{
     res.send("Lista dei post")
 })
 
+//show
+routers.get("/:id", (req, res)=>{
+    res.send(`Dettaglio del post: ${req.params.id}`)
+})
+
 module.exports = routers;
