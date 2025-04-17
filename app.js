57d0625316f632ab4f18026fa6524a50f6610c3e
danielemurgioni <dashomew@gmail.com>
2025-04-17 14:47:03 +0200

    [UPD] install express and server test

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-console.log("hello world");
+//importo express
+const express = require("express");
+//assegno una funzione a una variabile
+const app = express();
+//assegno la porta
+const port = 3000;
+
+// rotta madre
+app.get("/", (req, res)=>{
+    res.send("Server del mio blog");
+});
+
+//il server rimane in ascolto
+app.listen(port, ()=>{
+    console.log(`Il server è in ascolto alla porta ${port}`);
+});
